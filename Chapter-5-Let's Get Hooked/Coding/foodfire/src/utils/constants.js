716bbd0e8c816bb3e1264dd89c7368d1b601f4dd
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-01-23 00:08:23 +0530

    🐛 Latest fix for display menu

--- Chapter-5-Let's Get Hooked/Coding/foodfire/src/utils/constants.js
+++ Chapter-5-Let's Get Hooked/Coding/foodfire/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CON_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 
-export const MENU_API = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+export const MENU_API = "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D12.9351929%26lng%3D77.62448069999999%26restaurantId%3D"
 
 export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
